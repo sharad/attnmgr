731714165c8dfce097a00cd0e04205fbe66ee613
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-07-18 23:14:27 +0530

    correction

--- attnmgr.py
+++ attnmgr.py
@@ -126,12 +126,127 @@ class Daemon(DaemonBase):
             tableKv = json.loads(msg)
             self.processJson(tableKv)
 
-    def loop(self):
+    def loop1(self):
         while True:
             # Wait for a connection
             self.log.warning('waiting for a connection')
             connection, client_address = self.sock.accept()
             self.processConnection(connection, client_address)
+
+    def loop(self):
+        # https://pymotw.com/2/select/
+        # Sockets from which we expect to read
+        inputs = [ server ]
+
+        # Sockets to which we expect to write
+        outputs = [ ]
+
+        # Connections are added to and removed from these lists by the server
+        # main loop. Since this version of the server is going to wait for a
+        # socket to become writable before sending any data (instead of
+        # immediately sending the reply), each output connection needs a queue
+        # to act as a buffer for the data to be sent through it.
+
+        # Outgoing message queues (socket:Queue)
+        message_queues = {}
+
+        # The main portion of the server program loops, calling select() to
+        # block and wait for network activity.
+
+        while inputs:
+
+            # Wait for at least one of the sockets to be ready for processing
+            self.log.warning('\nwaiting for the next event')
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+            # select() returns three new lists, containing subsets of the
+            # contents of the lists passed in. All of the sockets in the
+            # readable list have incoming data buffered and available to be
+            # read. All of the sockets in the writable list have free space in
+            # their buffer and can be written to. The sockets returned in
+            # exceptional have had an error (the actual definition of
+            # “exceptional condition” depends on the platform).
+
+            # The “readable” sockets represent three possible cases. If the
+            # socket is the main “server” socket, the one being used to listen
+            # for connections, then the “readable” condition means it is ready
+            # to accept another incoming connection. In addition to adding the
+            # new connection to the list of inputs to monitor, this section
+            # sets the client socket to not block.
+
+            # Handle inputs
+            for s in readable:
+
+                if s is server:
+                    # A "readable" server socket is ready to accept a connection
+                    connection, client_address = s.accept()
+                    self.log.warning('new connection from', client_address)
+                    connection.setblocking(0)
+                    inputs.append(connection)
+
+                    # Give the connection a queue for data we want to send
+                    message_queues[connection] = Queue.Queue()
+
+                    # The next case is an established connection with a client
+                    # that has sent data. The data is read with recv(), then
+                    # placed on the queue so it can be sent through the socket
+                    # and back to the client.
+                else:
+                    data = s.recv( Daemon.sockbuffLen )
+                    if data:
+                        # A readable client socket has data
+                        self.log.warning('received "%s" from %s' % (data, s.getpeername()))
+                        message_queues[s].put(data)
+                        # Add output channel for response
+                        if s not in outputs:
+                            outputs.append(s)
+
+                    # A readable socket without data available is from a client
+                    # that has disconnected, and the stream is ready to be
+                    # closed.
+                    else:
+                        # Interpret empty result as closed connection
+                        self.log.warning('closing %s after reading no data' % client_address)
+                        # Stop listening for input on the connection
+                        if s in outputs:
+                            outputs.remove(s)
+                        inputs.remove(s)
+                        s.close()
+
+                        # Remove message queue
+                        del message_queues[s]
+
+            # There are fewer cases for the writable connections. If there is
+            # data in the queue for a connection, the next message is sent.
+            # Otherwise, the connection is removed from the list of output
+            # connections so that the next time through the loop select() does
+            # not indicate that the socket is ready to send data.
+
+            # Handle outputs
+            for s in writable:
+                try:
+                    next_msg = message_queues[s].get_nowait()
+                except Queue.Empty:
+                    # No messages waiting so stop checking for writability.
+                    self.log.warning('output queue for %s is empty' % s.getpeername())
+                    outputs.remove(s)
+                else:
+                    self.log.warning('sending "%s" to %s' % (next_msg, s.getpeername()))
+                    s.send(next_msg)
+
+            # Finally, if there is an error with a socket, it is closed.
+
+            # Handle "exceptional conditions"
+            for s in exceptional:
+                self.log.warning('handling exceptional condition for %s' % s.getpeername())
+                # Stop listening for input on the connection
+                inputs.remove(s)
+                if s in outputs:
+                    outputs.remove(s)
+                s.close()
+
+                # Remove message queue
+                del message_queues[s]
 
 
 class Handler(DaemonBase):
