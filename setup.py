860f58ef55d82f9230ff3e61da1c7c1c09541259
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-07-28 00:39:45 +0530

    correction

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 # https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
 from setuptools import setup
 
-setup(name='attnmgr',
-      version='1.0',
+setup(name             = 'attnmgr',
+      version          = '1.0',
       # list folders, not files
-      packages=['attnmgr'],
-      scripts=['attnmgr/bin/attnmgr.py', 'attnmgr/bin/reqattn.py'],
-      package_data={'attnmgr': ['data/shell.zsh']},
-      )
+      packages         = ['attnmgr'],
+      scripts          = ['attnmgr/bin/attnmgr.py', 'attnmgr/bin/reqattn.py'],
+      package_data     = {'attnmgr': ['data/shell.zsh']},
+      install_requires = [])
