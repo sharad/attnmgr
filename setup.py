c91843e027af76c2afd849ec284386c79d512372
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-08-10 19:13:42 +0530

    corretion

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
-from setuptools import setup
+from setuptools
 
-setup(name             = 'attnmgr',
-      version          = '1.0',
-      # list folders, not files
-      # packages         = ['attnmgr'],
-      scripts          = ['bin/attnmgr', 'bin/reqattn'],
-      # package_data     = {'attnmgr': ['data/shell.zsh']},
-      data_files       = [('share/attnmgr', ['share/attnmgr-hook.zsh'])],
-      package_dir={"": "src"},
-      packages=setuptools.find_packages(where="src"),
-      install_requires = [])
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name             = 'attnmgr',
+                 version          = '1.0',
+                 # list folders, not files
+                 # packages         = ['attnmgr'],
+                 scripts          = ['bin/attnmgr', 'bin/reqattn'],
+                 # package_data     = {'attnmgr': ['data/shell.zsh']},
+                 data_files       = [('share/attnmgr', ['share/attnmgr-hook.zsh'])],
+                 package_dir={"": "src"},
+                 packages=setuptools.find_packages(where="src"),
+                 install_requires = [])
