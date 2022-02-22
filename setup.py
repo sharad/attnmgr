bc2e354a6016357157eaf730d9523ff715fc5273
Sharad Pratap <110339+sharad@users.noreply.github.com>
2022-02-22 17:51:32 +0530

    correction

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name             = 'attnmgr',
-                 version          = '2.0',
+                 version          = '3.0',
                  # list folders, not files
                  # packages         = ['attnmgr'],
                  scripts          = ['bin/attnmgr', 'bin/reqattn'],
                  # package_data     = {'attnmgr': ['data/shell.zsh']},
-                 data_files       = [('share/attnmgr', ['share/attnmgr-hook.zsh'])],
+                 data_files       = [('share', ['share/attnmgr-hook.zsh'])],
                  package_dir={"": "src"},
                  packages=setuptools.find_packages(where="src"),
                  install_requires = [])
